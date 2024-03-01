56918f54ea8f4ff08ce49785d0263884a0ffca3a
3sulton <71450562+3sulton@users.noreply.github.com>
2024-03-01 12:11:40 +0700

    feat: add users service

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
-const songs = require('./api/songs');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
-const SongsValidator = require('./validator/songs');
-const ClientError = require('./exceptions/ClientError');
+const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
-  const albumsService = new AlbumsService();
-  const songsService = new SongsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -20,22 +21,34 @@ const init = async () => {
       },
     },
   });
- 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumsValidator,
-    },
-  });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const usersService = new UsersService();
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
     },
-  });
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
